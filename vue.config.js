9ebb84611d19b05ece1ecf1565af47303ff88880
Flor Espino <florencia.espino@gmail.com>
2021-12-27 15:20:26 -0300

    vue config

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,5 @@
 module.exports = {
-  devServer: {
-    host: '0.0.0.0',
-    hot: true,
-    disableHostCheck: true,
-  },
   transpileDependencies: [
     'vuetify'
-  ],
-  publicPath: "/"
-    ? 'dev/'
-    : '/',
-  outputDir: process.env.VUE_BUILD_FOLDER,
-  // assetsDir: 'assets/',
-  indexPath: 'index.html',
-  // crossorigin: 'url-de-origen/',
-
+  ]
 }
